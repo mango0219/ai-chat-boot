895314b31cd14c3a0aa425483d6f46761c8046df
shihw <shihw_0219@163.com>
2024-08-21 23:36:15 +0800

    init project

---
+++ src/main/java/com/mango/bootchat/BootChatApplication.java
@@ -0,0 +1,13 @@
+package com.mango.bootchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mango/bootchat/BootChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mango.bootchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
